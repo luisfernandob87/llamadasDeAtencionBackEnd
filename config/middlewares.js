e61a34da3cac232ccf8c39e55cc257f369f954cb
luisfernandob87 <luisfernandob87@gmail.com>
2023-05-30 18:24:09 -0600

    changes cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,15 +2,6 @@ module.exports = [
   "strapi::errors",
   "strapi::security",
   "strapi::cors",
-  {
-    name: "strapi::cors",
-    config: {
-      origin: "*",
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
-      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-      keepHeaderOnError: true,
-    },
-  },
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
