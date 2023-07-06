27dd1fb392414699a3c4a729c7f5332b8231dffa
Luis Morales <luisfernandob87@gmail.com>
2023-07-05 21:44:01 -0600

    add Cartera

---
+++ src/api/cartera/controllers/cartera.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cartera controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cartera.cartera');
---
+++ src/api/cartera/routes/cartera.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cartera router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::cartera.cartera');
